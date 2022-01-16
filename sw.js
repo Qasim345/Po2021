1607fb26be7a5673e2e1e4b3f5b545a85c72ae7d
Qasim <69038179+Qasim345@users.noreply.github.com>
2022-01-16 16:00:20 +0430

    update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("static").then(cache => {
-      return cache.addAll(["/", "style.css", "script.js","Resume.zip","Profile.jpg","header.jpg","host.jpg","king.jpg","project.jpg","web_design.jpg","img/1.jpg","img/2.jpg","img/3.jpg","img/4.jpg","img/5.jpg","img/6.jpg","img/7.jpg","img/8.jpg","img/9.jpg","img/10.jpg"])
+      return cache.addAll(["/", "style.css", "script.js","img/q-logo.png"])
     })
   );
 });
