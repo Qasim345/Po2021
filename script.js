82ac57942c0408e700e6e79a9cdc8c6932609ed7
Qasim <69038179+Qasim345@users.noreply.github.com>
2022-01-16 16:10:14 +0430

    update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // pwa
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("sw.js").then(registeration => {
-    console.log("SW Registered!");
-    console.log(registeration);
+    /*console.log("SW Registered!");
+    console.log(registeration);*/
   }).catch(error => {
-    console.log("SW Registered filed");
+    //console.log("SW Registered filed");
     console.log(error);
   })
 }
